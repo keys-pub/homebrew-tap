8b0163475dd09adcc9c08227f337ed9377597dfb
Gabriel Handford <gabrielh@gmail.com>
2020-02-21 09:27:59 -0800

    Update keys.rb

--- Casks/keys.rb
+++ Casks/keys.rb
@@ -1,6 +1,6 @@
 cask 'keys' do
     version '0.0.16'
-    sha256 '351ffa35830f5dc0e4c6506c2cc52ed5cb7b45e8745723657d6d833462e5c9bb'
+    sha256 'e382d84acc7ddd8a4fb36bd71bfb1bbf503a8538e6b359229c888deca09bb97b'
   
     url "https://github.com/keys-pub/app/releases/download/v#{version}/Keys-#{version}-mac.zip"
     appcast 'https://github.com/keys-pub/app/releases.atom'
