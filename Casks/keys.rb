ef911748730a8513a903de5c3617d21d2bec968c
Gabriel Handford <gabrielh@gmail.com>
2020-03-10 12:13:15 -0700

    Update Casks/keys.rb (0.0.20)

--- Casks/keys.rb
+++ Casks/keys.rb
@@ -1,14 +1,13 @@
 cask 'keys' do
     version '0.0.20'
     sha256 '5d91ba34e103313aef6364ca96aa329dd3abd9d749d1ff27b6d2097389423d60'
-  
-    url "https://github.com/keys-pub/app/releases/download/v#{version}/Keys-#{version}-mac.zip"
+
+    url "https://github.com/keys-pub/app/releases/download/v0.0.20/Keys-0.0.20-mac.zip"
     name 'Keys'
     homepage 'https://keys.pub'
 
-    auto_updates true
     depends_on macos: '>= :sierra'
-  
+
     app 'Keys.app'
 
     uninstall delete: [
