9d6c4fadbeb8a2d6e7de89eb0a2eac9e8a558c30
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-13 21:17:34 -0700

    Brew formula update for keys version v0.0.31

--- keys.rb
+++ keys.rb
@@ -2,12 +2,12 @@
 class Keys < Formula
   desc ""
   homepage "https://keys.pub"
-  version "0.0.30"
+  version "0.0.31"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/keys-pub/keysd/releases/download/v0.0.30/keys_0.0.30_darwin_x86_64.tar.gz"
-    sha256 "baf95f245406e3090cbe12f87fb3d5a9fbc07ee1338f2edaa842134952069be7"
+    url "https://github.com/keys-pub/keysd/releases/download/v0.0.31/keys_0.0.31_darwin_x86_64.tar.gz"
+    sha256 "9c37ddd884d2d9558d635b5882f3c888551997f7ed36f57abd6643693dbb8fde"
   elsif OS.linux?
   end
 
