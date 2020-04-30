45456f3cb11b41cdb5d2608379f7c8229ff7c58b
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-29 19:47:19 -0700

    Brew formula update for keys version v0.0.38

--- keys.rb
+++ keys.rb
@@ -2,17 +2,18 @@
 class Keys < Formula
   desc ""
   homepage "https://keys.pub"
-  version "0.0.37"
+  version "0.0.38"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/keys-pub/keysd/releases/download/v0.0.37/keys_0.0.37_darwin_x86_64.tar.gz"
-    sha256 "a03ddf18eb9231f64fe394694728ce3873e157ebcf4507b4814a11b5849c393c"
+    url "https://github.com/keys-pub/keysd/releases/download/v0.0.38/keys_0.0.38_darwin_x86_64.tar.gz"
+    sha256 "121824237f3322de7db1138e5a71e133151cd7af4d57f150c4200f4af527baa6"
   elsif OS.linux?
   end
 
   def install
     bin.install "keys"
     bin.install "keysd"
+    bin.install "fido2.so"
   end
 end
