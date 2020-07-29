55a3971d240c125633e4e41915822f041ae88b5a
Gabriel Handford <gabrielh@gmail.com>
2020-07-29 14:16:43 -0700

    Update Casks/keys.rb (0.1.7)

--- Casks/keys.rb
+++ Casks/keys.rb
@@ -1,8 +1,8 @@
 cask 'keys' do
-    version '0.0.48'
-    sha256 'ad04126b1dee2780723d37fe3651bc3e7b91b5b9da7622676e2e3af9461563ad'
+    version '0.1.7'
+    sha256 '313b69a26c7ce75f0f5277721c3655f92ba44d8e750f54a36e0b50b53445dd98'
 
-    url "https://github.com/keys-pub/app/releases/download/v0.0.48/Keys-0.0.48-mac.zip"
+    url "https://github.com/keys-pub/app/releases/download/v0.1.7/Keys-0.1.7-mac.zip"
     name 'Keys'
     homepage 'https://keys.pub'
 
