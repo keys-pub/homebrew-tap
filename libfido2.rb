4e230392f057498d0eff31aa3998e030c5134d92
Gabriel Handford <gabrielh@gmail.com>
2020-09-13 15:08:22 -0700

    Use keys-pub/libfido2

--- libfido2.rb
+++ libfido2.rb
@@ -1,29 +1,15 @@
 class Libfido2 < Formula
     desc "Provides library functionality for FIDO U2F & FIDO 2.0, including USB"
     homepage "https://developers.yubico.com/libfido2/"
-    url "https://github.com/Yubico/libfido2/archive/1.4.0.tar.gz"
-    sha256 "ad921fbe7d4bb70e4a971e564cd01f341daf9b5ed5d69b3cbab94a8a811d2a6c"
-    license "BSD-2-Clause"
-    revision 2
-  
-    bottle do
-      cellar :any
-      sha256 "ba0312771c9cab19b0081ce397e34064d7a203856344d2364f2e6650ca1b3ed3" => :catalina
-      sha256 "4fab8b470d78e69569383566b4dd77e78279e72f69ffafb20548842669731471" => :mojave
-      sha256 "64ea1c57c33c2cf778fd58ad0bd9f033c1b60e9d012a810a467fd5a9438afe14" => :high_sierra
-    end
-  
+    url "https://github.com/keys-pub/libfido2/archive/1.5.1.tar.gz"
+    sha256 "5dae88011aca0f123b2fed031db887f8e6ee2b7d4b48d84e415ee1ce5f801ac9"
+    license "BSD-2-Clause"    
+      
     depends_on "cmake" => :build
     depends_on "mandoc" => :build
     depends_on "pkg-config" => :build
     depends_on "libcbor"
     depends_on "openssl@1.1"
-
-    # Apply fix for https://github.com/Yubico/libfido2/issues/166 (also https://github.com/Yubico/libfido2/issues/179)
-    patch do
-      url "https://github.com/Yubico/libfido2/commit/39544a2c342b0438a8f341b4a4ff20f650f701a3.diff?full_index=1"
-      sha256 "664a95d68502a266835839002d32149ae391aef5b902d33a990c00539a68fe32"
-    end
 
     def install
       mkdir "build" do
