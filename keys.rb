7ed67cb05dffcd7b25c1243bf678a64dbd5ffb2a
Gabriel Handford <gabrielh@gmail.com>
2020-09-18 14:32:06 -0700

    Update keys.rb (0.1.18)

--- keys.rb
+++ keys.rb
@@ -1,12 +1,12 @@
 class Keys < Formula
   desc "Key management"
   homepage "https://keys.pub"
-  version "0.1.16"
+  version "0.1.18"
   bottle :unneeded
   
   if OS.mac?
-    url "https://github.com/keys-pub/keys-ext/releases/download/v0.1.16/keys_0.1.16_darwin_x86_64.tar.gz"
-    sha256 "e6e882272c9f3e8c6c431aa091e19c96ae8c7aed2bed63c8a37dda9ffa107110"
+    url "https://github.com/keys-pub/keys-ext/releases/download/v0.1.18/keys_0.1.18_darwin_x86_64.tar.gz"
+    sha256 "3d4bda9501f122064b3a3ae0cf3ff99fab7b17ee03859086a04a4eca61f62250"
   elsif OS.linux?
   end
   
